88cc869d2e1e2181bceece49bea473f2deca5c60
mcj010 <329392494@qq.com>
2020-09-21 22:06:33 +0800

    加了 sleuth 链路追踪器

--- src/main/java/com/example/eurekaClient/controller/ServerProvideController.java
+++ src/main/java/com/example/eurekaClient/controller/ServerProvideController.java
@@ -1,6 +1,8 @@
 package com.example.eurekaClient.controller;
 
-import com.example.eurekaClient.model.TestModel;
+import javax.servlet.http.HttpServletRequest;
+
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.MediaType;
@@ -8,19 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import com.example.eurekaClient.model.TestModel;
 
 @RestController
 public class ServerProvideController {
     //描述eureka客户端信息的类
     @Autowired
     private DiscoveryClient eurekaClient;
+    
+    private final Logger logger = Logger.getLogger(getClass());
 
     @RequestMapping(value="/ServerTest", method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
     public TestModel getTest(HttpServletRequest request){
         TestModel tm = new TestModel();
         tm.setUrl(request.getRequestURL().toString());
         tm.setMsg("请求访问成功");
+        logger.info("ServerProvideController");
         return tm;
     }
 }
